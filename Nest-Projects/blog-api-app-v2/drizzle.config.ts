0e5bf3421e06d78a71972f34795f3836863a673c
Or Ben Naim <orbennaim123@gmail.com>
2024-12-17 08:32:22 +0200

    .

--- Nest-Projects/blog-api-app-v2/drizzle.config.ts
+++ Nest-Projects/blog-api-app-v2/drizzle.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    schema: "./src/drizzle/schema.ts",
-    dialect: 'postgresql'
-})
+  schema: "./src/drizzle/schema.ts", // Path to the schema file
+  out: "./drizzle", // Path to store migrations
+  driver: "pglite", // PostgreSQL driver
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL as string
+  },
+  verbose: true,
+  strict: true
+});
--- Nest-Projects/blog-api-app-v2/src/blog/dto/create-blog.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CreateBlogDtoTs } from './create-blog.dto.js';
-
-describe('CreateBlogDtoTs', () => {
-  it('should be defined', () => {
-    expect(new CreateBlogDtoTs()).toBeDefined();
-  });
-});
