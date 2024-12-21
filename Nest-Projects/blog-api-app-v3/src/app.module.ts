6d1fbb800c378d49f4b5c22a3157a8f1f4d78a98
Or Ben Naim <orbennaim123@gmail.com>
2024-12-21 16:45:32 +0200

    .

--- Nest-Projects/blog-api-app-v3/src/app.module.ts
+++ Nest-Projects/blog-api-app-v3/src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule } from '@nestjs/config';
 import { BlogController } from './api/blog.controller';
-import { CreateBlogHandler } from './application/handlers/create-blog.handler';
-import { GetAllBlogsHandler } from './application/handlers/get-all-blogs.handler';
-import { GetBlogByIdHandler } from './application/handlers/get-blog-by-id.handler';
+import { CommandHandlers, QueryHandlers } from 'src/application/handlers/all.handlers';
 import { DrizzleBlogRepository } from 'src/infrastrucature/repositories/drizzle-blog-repository';
 import { DatabaseModule } from 'src/infrastrucature/database/db.module';
 import { BLOG_REPOSITORY } from 'src/infrastrucature/repositories/blog-repository-interface';
-import { GetJokeHandler } from './application/handlers/get-joke.handler';
-import { DeleteBlogByIdHandler } from './application/handlers/delete-blog.handler';
-import { DeleteAllBlogHandler } from './application/handlers/delete-all-blogs.handler';
+
 
 
 @Module({
@@ -24,12 +20,8 @@ import { DeleteAllBlogHandler } from './application/handlers/delete-all-blogs.ha
   controllers: [BlogController],
   providers: [
     { provide: BLOG_REPOSITORY, useClass: DrizzleBlogRepository },
-    CreateBlogHandler,
-    GetAllBlogsHandler,
-    GetBlogByIdHandler,
-    GetJokeHandler,
-    DeleteBlogByIdHandler,
-    DeleteAllBlogHandler
+    ...CommandHandlers,
+    ...QueryHandlers,
   ],
 })
 export class AppModule {}
