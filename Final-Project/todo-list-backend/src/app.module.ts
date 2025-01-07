2502064d03789841dd9f3958c4a187198de877f3
Or Ben Naim <orbennaim123@gmail.com>
2025-01-07 17:36:54 +0200

    .

--- Final-Project/todo-list-backend/src/app.module.ts
+++ Final-Project/todo-list-backend/src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule } from '@nestjs/config';
-import { ToDoListController } from './todos/api/ToDoList.controller';
-import { CommandHandlers, QueryHandlers } from 'src/todos/application/handlers/all.handlers';
-import { ToDoListRepository } from 'src/todos/infrastructure/repositories/toDoList.repository';
 import { DatabaseModule } from 'src/database/db.module';
-import { TODOLIST_REPOSITORY } from 'src/todos/infrastructure/repositories/toDoList.repository-interface';
-import { AuthController } from './users/auth.controller';
-import { AuthModuleOptions } from '@nestjs/passport';
 import { AuthModule } from './auth/auth.module';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
