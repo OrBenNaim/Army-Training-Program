7ad0e6d16562140447e42733bb6ca4a35c0286aa
Or Ben Naim <orbennaim123@gmail.com>
2025-02-13 23:02:11 +0200

    .

--- Fleet-Vehicles-Management/backend/drizzle.config.ts
+++ Fleet-Vehicles-Management/backend/drizzle.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'drizzle-kit';
+// import { defineConfig } from 'drizzle-kit';
 
-export default defineConfig({
-    schema: [
-        './src/database/schemas/vehicles.ts', 
-        './src/database/schemas/fleets.ts',
-        './src/database/schemas/vehicleStatusHistory.ts',
-    ],
-    out: './drizzle',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL
-    },
-});
+// export default defineConfig({
+//     schema: [
+//         './src/database/schemas/vehicles.ts', 
+//         './src/database/schemas/fleets.ts',
+//         './src/database/schemas/vehicleStatusHistory.ts',
+//     ],
+//     out: './drizzle',
+//     dialect: 'postgresql',
+//     dbCredentials: {
+//         url: process.env.DATABASE_URL
+//     },
+// });
